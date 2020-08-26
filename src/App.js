ad1822090f62658eddd3f8714bf261b082f82948
Rômullo S <romullo_sas@outlook.com>
2020-08-26 03:31:51 -0300

    Deploy hotfiex-6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import AppProvider from './hooks';
-
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 
@@ -12,10 +10,8 @@ import GlobalStyle from './styles/global';
 const App = () => (
   <>
     <BrowserRouter>
-      <AppProvider>
         <ScrollToTop/>
         <Routes />
-      </AppProvider>
       <GlobalStyle />
     </BrowserRouter>
   </>
